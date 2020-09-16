fad1ae9907e07dfb08cc9ede358638707f1a7b40
Zach <zquinn617@utexas.edu>
2020-09-16 17:16:07 -0500

    setup for Login Controller

---
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.revature.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.revature.models.User;
+import com.revature.services.UserService;
+import com.revature.templates.LoginTemplate;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+	@Autowired
+	private UserService service;
+	
+	@PostMapping(path = "login", produces = "application/json")
+	@ResponseBody
+	public User loginController(@RequestBody LoginTemplate lf) {
+		return service.login(lf);
+	}
+	
+}
--- src/main/java/com/revature/repositories/IRecipeDAO.java
+++ src/main/java/com/revature/repositories/IRecipeDAO.java
@@ -1,5 +1,11 @@
 package com.revature.repositories;
 
+import java.util.Set;
+
+import com.revature.models.Recipe;
+
 public interface IRecipeDAO {
+
+	Set<Recipe> findAll();
 
 }
