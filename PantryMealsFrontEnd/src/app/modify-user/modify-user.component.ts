23240bbbac5a2809fe057f23d1f5ab662f909a95
arlukens20 <Drew4818@gmail.com>
2020-09-23 11:12:23 -0400

    Added Styling fixes

--- PantryMealsFrontEnd/src/app/modify-user/modify-user.component.ts
+++ PantryMealsFrontEnd/src/app/modify-user/modify-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UserServicesService } from 'src/app/services/user-services.service';
 
 @Component({
   selector: 'app-modify-user',
@@ -15,15 +16,17 @@ export class ModifyUserComponent implements OnInit {
     password: new FormControl(''),
   });
 
+  userService = UserServicesService;
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
   update() {
+    //this.userService.updateUser(this.modifyForm.value);
     console.warn(this.modifyForm.value);
   }
   goBack() {
-    this.router.navigate(['profile']);
+    this.router.navigate(['home']);
     console.warn('Log Out function needed');
   }
 }
--- PantryMealsFrontEnd/src/app/services/user-services.service.ts
+++ PantryMealsFrontEnd/src/app/services/user-services.service.ts
@@ -1,9 +1,19 @@
+import { HttpBackend } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserServicesService {
+  private baseUrl: String = 'http://localhost:8080/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  updateUser(value: String): Observable<User> {
+    return this.http.post<User>(this.baseUrl + '/updateUsers/', value);
+  }
 }
