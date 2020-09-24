c303adfca6de2cd6ae4cb613455019dc3e6c3388
Leron Charles <lc443@njit.edu>
2020-09-24 10:40:44 -0400

    started Selenium testing. Created class Login page

---
+++ PantryMealsBackEnd/src/test/java/com/revature/pages/TopChefsLogin.java
@@ -0,0 +1,37 @@
+package com.revature.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TopChefsLogin {
+	public final String title = "Login";
+
+	@FindBy(xpath = "//input[@name='username']")
+	public WebElement username;
+	
+	@FindBy(name = "password")
+	public WebElement password;
+	
+	@FindBy(xpath = "//input[@type='submit']")
+	public WebElement submitButton;
+
+	@FindBy(linkText="Home")
+	public WebElement homeLink;
+	
+	@FindBy(linkText="Login")
+	public WebElement loginLink;
+	
+	
+	public TopChefsLogin(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void loginToTopChefs(String username, String password) {
+		this.username.sendKeys(username);
+		this.password.sendKeys(password);
+		this.submitButton.click();
+	}
+	
+}
