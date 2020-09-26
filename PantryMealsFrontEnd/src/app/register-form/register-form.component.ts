83ad449f874eba4f972eac140362b37028d273ab
arlukens20 <Drew4818@gmail.com>
2020-09-26 12:00:04 -0400

    Work on Register new Users

--- PantryMealsFrontEnd/src/app/register-form/register-form.component.ts
+++ PantryMealsFrontEnd/src/app/register-form/register-form.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service'
 import { environment } from '../../environments/environment';
+import { User } from '../models/User';
+
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
@@ -10,28 +13,43 @@ import { environment } from '../../environments/environment';
 })
 export class RegisterFormComponent implements OnInit {
   registerForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
+    Username: new FormControl(''),
+    Password: new FormControl(''),
+    FirstName: new FormControl(''),
+    LastName: new FormControl(''),
+    Email: new FormControl(''),
   });
   Auth: AuthenticationService;
+  baseURL = environment.baseURL;
+  constructor(private router: Router, private http: HttpClient) { }
 
-  constructor(private router: Router) { }
+  private id: number;
+  private username: String;
+  private firstName: String;
+  private lastName: String;
+  private email: String;
+  private password: String;
 
   ngOnInit(): void { }
 
   onSubmit() {
     try {
-      console.warn(this.registerForm.value);
-      this.Auth.registerService(this.registerForm.get('username').value, this.registerForm.get('password').value, this.registerForm.get('firstName').value,
-        this.registerForm.get('lastName').value, this.registerForm.get('email').value);
-      this.router.navigate(['login']);
+      let results = this.http.post<User>(
+        this.baseURL + 'newUser',
+        {
+          username: this.registerForm.get('Username').value,
+          password: this.registerForm.get('Password').value,
+          firstName: this.registerForm.get('FirstName').value,
+          LastName: this.registerForm.get('LastName').value,
+          email: this.registerForm.get('Email').value,
+        }
+      );
+      console.log(results);
     } catch (error) {
-      console.warn("there was an error");
+      console.log(error);
+      alert('Failed to submit');
     }
-
-
+    console.warn(this.registerForm.value);
+    this.router.navigate(['login']);
   }
 }
