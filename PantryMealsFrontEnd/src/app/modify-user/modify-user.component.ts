163b39b8c0e2109e3b95436218ed2a121c6545a3
arlukens20 <Drew4818@gmail.com>
2020-09-26 09:47:12 -0400

    Still working on Modify Users

--- PantryMealsFrontEnd/src/app/modify-user/modify-user.component.ts
+++ PantryMealsFrontEnd/src/app/modify-user/modify-user.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserServicesService } from 'src/app/services/user-services.service';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/User';
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-modify-user',
   templateUrl: './modify-user.component.html',
@@ -12,18 +15,47 @@ export class ModifyUserComponent implements OnInit {
   modifyForm = new FormGroup({
     FirstName: new FormControl(''),
     LastName: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
+    Email: new FormControl(''),
+    Password: new FormControl(''),
   });
 
-  userService = UserServicesService;
-  constructor(private router: Router) {}
+  private id: number;
+  private username: String;
+  private firstName: String;
+  private lastName: String;
+  private email: String;
+  private password: String;
+  baseUrl = environment.baseURL;
+  // userService = UserServicesService;
+  constructor(private router: Router, private http: HttpClient) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
-  update() {
-    //this.userService.updateUser(this.modifyForm.value);
-    console.warn(this.modifyForm.value);
+  async update() {
+    let cu = sessionStorage.getItem("currentUser");
+    let User = JSON.parse(cu);
+    //console.log(User);
+
+    //Doesn't work at the moment
+
+    try {
+      let user = await this.http.post<User>(
+        this.baseUrl + 'updateUser',
+        {
+          id: User.id,
+          username: User.username,
+          password: this.modifyForm.get('Password'),
+          firstName: this.modifyForm.get('FirstName'),
+          LastName: this.modifyForm.get('LastName'),
+          email: this.modifyForm.get('Email'),
+        }
+      ).toPromise();
+
+      console.warn(this.modifyForm.value);
+      this.router.navigateByUrl("/home");
+    } catch (error) {
+      alert("Update was unsuccessful.");
+    }
   }
   goBack() {
     this.router.navigateByUrl("/home");
